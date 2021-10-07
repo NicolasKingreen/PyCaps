10f351633bd165339e7e73719e6e0d4ec4eb7d4b
Nicolas Kingreen <NicolasKi>
2021-10-07 14:48:56 +0300

    testing collisions

--- caps.py
+++ caps.py
@@ -9,7 +9,7 @@ pygame.font.init()
 FONT = pygame.font.SysFont(None, 16)
 DRAW_DEBUG = False
 
-CAPS_SPAWNED = 360
+CAPS_SPAWNED = 4
 
 
 def draw_text(text, topleft=None, center=None):
@@ -59,6 +59,7 @@ class Cap:
         self.acceleration = Cap.ACCELERATION
         self.color = get_random_color()
         self.radius = Cap.get_random_radius()
+        self.mass = self.radius / 8
 
     def __str__(self):
         return f"Cap: \n" \
@@ -73,7 +74,7 @@ class Cap:
         y = int(self.coordinates.y)
         dx = self.moving_direction.x
         dy = self.moving_direction.y
-        return f"Cap({x}, {y})({dx:.2f}, {dy:.2f})"
+        return f"Cap({x}, {y})"
 
     def update(self, frame_time_s):
         self.speed += self.acceleration * frame_time_s
@@ -100,11 +101,14 @@ class Cap:
             self.coordinates.y = Window.HEIGHT - self.radius
             self.moving_direction.y *= -1
 
-    def draw(self, surface):
+    def draw(self, surface, frame_time_s):
         pygame.draw.circle(surface, self.color, self.coordinates, self.radius)
         if DRAW_DEBUG:
+            text_surf = FONT.render(f"Circle({self.mass})", True, (255, 0, 0))
+            text_rect = text_surf.get_rect(center=(self.coordinates.x, self.coordinates.y - self.radius - 10))
             pygame.draw.circle(surface, (255, 0, 0), self.coordinates, self.radius, 1)
-            pygame.draw.line(surface, (255, 0, 0), self.coordinates, self.coordinates + self.moving_direction * self.speed / 4)
+            pygame.draw.line(surface, (255, 0, 0), self.coordinates, self.coordinates + self.moving_direction * self.speed * frame_time_s)
+            surface.blit(text_surf, text_rect)
 
     def distance_to(self, other_cap):
         return self.coordinates.distance_to(other_cap.coordinates)
@@ -114,7 +118,7 @@ class Cap:
 
     @staticmethod
     def get_random_radius():
-        radius = random.randint(16, 32)
+        radius = random.randint(16, 128)
         return radius
 
     @staticmethod
@@ -143,7 +147,7 @@ class Window:
 
 class CapsGame:
 
-    MAX_FPS = 120
+    MAX_FPS = 60
 
     def __init__(self):
         self.clock = pygame.time.Clock()
@@ -173,17 +177,52 @@ class CapsGame:
                 for cap in self.caps:
                     cap.update(frame_time_s)
 
-            # caps collision
-            # for cap1 in self.caps:
-            #     for cap2 in self.caps:
-            #         if cap1 is not cap2:
-            #             if cap1.collides(cap2):
-            #                 cap1.moving_direction *= -1
-            #                 # cap1.moving_direction = cap1.moving_direction.cross(cap2.moving_direction)
-            #                 # cap1.moving_direction.normalize_ip()
+                # caps collision
+                for cap1 in self.caps:
+                    for cap2 in self.caps:
+                        if cap1 is not cap2:
+                            if cap1.collides(cap2):
+
+                                # TODO: process still caps
+
+                                # cap2.coordinates = cap1.coordinates.
+
+                                # mass_ratio = cap1.mass / cap2.mass
+                                #
+                                # new_vel_x1 = (cap1.moving_direction.x * cap1.speed * (cap1.mass - cap2.mass) + (2 * cap2.mass * cap2.moving_direction.x * cap2.speed))/(cap1.mass + cap2.mass)
+                                # new_vel_x1 /= mass_ratio
+                                # new_vel_y1 = (cap1.moving_direction.y * cap1.speed * (cap1.mass - cap2.mass) + (2 * cap2.mass * cap2.moving_direction.y * cap2.speed))/(cap1.mass + cap2.mass)
+                                # new_vel_y1 /= mass_ratio
+                                # new_vel_x2 = (cap2.moving_direction.x * cap1.speed * (cap2.mass - cap1.mass) + (2 * cap1.mass * cap1.moving_direction.x * cap1.speed))/(cap1.mass + cap2.mass)
+                                # new_vel_x2 *= mass_ratio
+                                # new_vel_y2 = (cap2.moving_direction.y * cap1.speed * (cap2.mass - cap1.mass) + (2 * cap1.mass * cap1.moving_direction.y * cap1.speed))/(cap1.mass + cap2.mass)
+                                # new_vel_y2 *= mass_ratio
+                                # velocity_vector1 = Vector(new_vel_x1, new_vel_y1)
+                                # velocity_vector2 = Vector(new_vel_x2, new_vel_y2)
+                                # new_speed1 = velocity_vector1.magnitude()
+                                # new_speed2 = velocity_vector2.magnitude()
+                                # if velocity_vector1:
+                                #     velocity_vector1.normalize_ip()
+                                # if velocity_vector2:
+                                #     velocity_vector2.normalize_ip()
+                                # cap1.moving_direction = velocity_vector1
+                                # cap2.moving_direction = velocity_vector2
+                                # cap1.speed = new_speed1
+                                # cap2.speed = new_speed2
+                                # cap1.coordinates += cap1.moving_direction * cap1.speed * frame_time_s
+                                # cap2.coordinates += cap2.moving_direction * cap2.speed * frame_time_s
+
+                                cap1_old_md = cap1.moving_direction
+                                cap2_old_md = cap2.moving_direction
+
+                                cap1.moving_direction.reflect_ip(cap2_old_md)
+                                cap1.coordinates += cap1.moving_direction * cap1.speed * frame_time_s * 2
+                                # cap1.moving_direction.reflect_ip(cap1_old_md)
+
+                                cap1.color = get_random_color()
 
             # graphics
-            self._draw_graphics()
+            self._draw_graphics(frame_time_s)
 
     def get_current_fps(self):
         return int(self.clock.get_fps())
@@ -202,6 +241,8 @@ class CapsGame:
                     DRAW_DEBUG = not DRAW_DEBUG
                 elif event.key == K_f:
                     self.window.toggle_fullscreen()
+                elif event.key == K_a:
+                    self.caps.append(Cap())
             elif event.type == MOUSEBUTTONDOWN:
                 if event.button == 1:
                     mouse_x, mouse_y = pygame.mouse.get_pos()
@@ -220,14 +261,14 @@ class CapsGame:
                         cap.moving_direction = new_direction_vector
                         cap.speed += min(1., 50/mouse_coordinates.distance_to(cap.coordinates)) * 250
 
-    def _draw_graphics(self):
+    def _draw_graphics(self, frame_time_s):
         render_surface = self.window.get_surface()
         render_surface.fill(pygame.Color("white"))
         for cap in self.caps:
-            cap.draw(render_surface)
+            cap.draw(render_surface, frame_time_s)
         if DRAW_DEBUG:
             draw_text(str(self.caps), topleft=(10, 10))
-            draw_text(str(self.get_current_fps()), topleft=(10, 26))
+            draw_text(f"{self.get_current_fps()} FPS", topleft=(10, 26))
         if self.is_paused:
             draw_text("Game is paused", center=(Window.WIDTH // 2, Window.HEIGHT // 2))
         pygame.display.update()
